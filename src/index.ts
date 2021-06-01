4e4383f4e8cc65182044942fa775b7e9fd47d184
Tormak <63308171+Tormak9970@users.noreply.github.com>
2021-06-01 13:32:44 -0500

    Updated index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,16 @@ export { default as getPatchZip } from './ssn/getPatchZip';
 export { default as getSolidpkg } from './ssn/getSolidpkg';
 export { default as installPatch } from './ssn/installPatch';
 export { default as verifyProductName } from './ssn/verify/verifyProductName';
+
+//added by Tormak
+export { default as extractFileAsStream } from './ssn/extractFileAsStream';
+export { default as parseBencode } from './ssn/reader/bencodeParser';
+export { default as readSsnFile } from './ssn/reader/readSsnFile';
+export { default as arrayBufferToStream } from './ssn/streams/arrayBufferToStream';
+export { default as readLocalFileHeader } from './ssn/streams/readLocalFileHeader';
+export { default as streamToArrayBuffer } from './ssn/streams/streamToArrayBuffer';
+export { default as verifySolidpkg } from './ssn/verify/verifySolidpkg';
+
+export { default as parsePatchmanifest } from './ssn/reader/parsePatchmanifest';
+export { default as streamToString } from './ssn/streams/streamToString';
+export { default as verifyPatchmanifest } from './ssn/verify/verifyPatchmanifest';
